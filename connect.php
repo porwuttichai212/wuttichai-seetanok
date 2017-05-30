30fbc54b7fa7a4b7638d5909058c9562bf34e7c2
porwuttichai212 <porwuttichai212@gmail.com>
2017-05-30 17:04:36 +0700

    kuypoooor

---
+++ connect.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body>
+ <?php
+   $conn=mysqli_connect("localhost","root","","d56");
+   if($conn){
+	   echo "connect sussecc";
+   }else{
+	   echo "ERROR CONNECT";
+     {
+?>
+ </body>
+</html>
+
+
+
+ 
